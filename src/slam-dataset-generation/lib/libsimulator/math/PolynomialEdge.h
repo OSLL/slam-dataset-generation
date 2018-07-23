421ad093e94aa7b8d695b9f5cf64d024f9116723
Jake Whitton <jwhitton@mit.edu>
2018-07-23 15:53:42 +0300

    Refactor PolynomialEdge constructor and update HermiteEdge to comply with changes

--- src/slam-dataset-generation/lib/libsimulator/math/PolynomialEdge.h
+++ src/slam-dataset-generation/lib/libsimulator/math/PolynomialEdge.h
@@ -8,69 +8,80 @@ template <int degree>
 class PolynomialEdge : public UnitParametricEdge
 {
 public:
-	PolynomialEdge() = default;
-
-	PolynomialEdge(Polynomial<degree> x, Polynomial<degree> y)
-		: x_(std::move(x))
-		, y_(std::move(y))
-	{ }
-
-	PolynomialEdge(Vec A, Vec B, Vec C, Vec D)
-		: x_(Polynomial<3>(D.x, C.x, B.x, A.x))
-		, y_(Polynomial<3>(D.y, C.y, B.y, A.y))
-	{ }
+	template <typename ... Args>
+	PolynomialEdge(Args ... args);
 
 	virtual ~PolynomialEdge() = default;
 
-	Vec operator()(double t) const
-	{
-		return {x_(t), y_(t)};
-	}
+	Vec operator()(double t) const;
+	Vec derivative(double t) const;
 
-	Vec derivative(double t) const
-	{
-		return {x_.derivative(t), y_.derivative(t)};
-	}
+	virtual void print(std::ostream & o, int tabs = 0) const;
+	void printVectorPolynomial(std::ostream & o = std::cout) const;
 
-	virtual void print(std::ostream & o, int tabs = 0) const
-	{
-		// Print correct number of tabs
-		for (int i = 0; i < tabs; i++)
-			o << '\t';
-		
-		o << "PolynomialEdge<" << degree << ">: ";
-
-		printVectorPolynomial(o);
-	}
-
-	void printVectorPolynomial(std::ostream & o = std::cout) const
-	{
-		const auto & x_coefficients = x_.getCoefficients();
-		const auto & y_coefficients = y_.getCoefficients();
-	
-		for (int i = degree; i >= 0; i--)
-		{
-			o << Vec {x_coefficients[i], y_coefficients[i]};
-
-			if (i != 0)
-				o << "t" << (i != 1 ? std::string("^") + std::to_string(i): "") <<  " + ";
-		}
-
-	}
-
-	void setX(Polynomial<degree> x)
-	{
-		x_ = std::move(x);
-	}
-
-	void setY(Polynomial<degree> y)
-	{
-		y_ = std::move(y);
-	}
 protected:
+
 	Polynomial<degree> x_;
+
 	Polynomial<degree> y_;
 };
+
+
+/* ========================= Constructor ========================= */
+template <int degree>
+template <typename ... Args>
+PolynomialEdge<degree>::PolynomialEdge(Args ... args)
+	: x_(Polynomial<degree>(args.x ...))
+	, y_(Polynomial<degree>(args.y ...))
+{ }
+/* =============================================================== */
+
+
+
+/* ========================= Constructor ========================= */
+template <int degree>
+Vec PolynomialEdge<degree>::operator()(double t) const
+{
+	return {x_(t), y_(t)};
+}
+
+template <int degree>
+Vec PolynomialEdge<degree>::derivative(double t) const
+{
+	return {x_.derivative(t), y_.derivative(t)};
+}
+/* =============================================================== */
+
+
+
+/* ========================= Constructor ========================= */
+template <int degree>
+void PolynomialEdge<degree>::print(std::ostream & o, int tabs) const
+{
+	// Print correct number of tabs
+	for (int i = 0; i < tabs; i++)
+		o << '\t';
+	
+	o << "PolynomialEdge<" << degree << ">: ";
+
+	printVectorPolynomial(o);
+}
+
+template <int degree>
+void PolynomialEdge<degree>::printVectorPolynomial(std::ostream & o) const
+{
+	const auto & x_coefficients = x_.getCoefficients();
+	const auto & y_coefficients = y_.getCoefficients();
+
+	for (int i = degree; i >= 0; i--)
+	{
+		o << Vec {x_coefficients[i], y_coefficients[i]};
+
+		if (i != 0)
+			o << "t" << (i != 1 ? std::string("^") + std::to_string(i): "") <<  " + ";
+	}
+
+}
 
 template <int degree>
 std::ostream & operator<<(std::ostream & o, const PolynomialEdge<degree> & f)
@@ -78,5 +89,6 @@ std::ostream & operator<<(std::ostream & o, const PolynomialEdge<degree> & f)
 	f.print(o);
 	return o;
 }
+/* =============================================================== */
 
 #endif
--- src/slam-dataset-generation/lib/libsimulator/trajectory/HermiteEdge.cpp
+++ src/slam-dataset-generation/lib/libsimulator/trajectory/HermiteEdge.cpp
@@ -30,9 +30,9 @@ void HermiteEdge::print(ostream & o, int tabs) const
 
 HermiteEdge::HermiteEdge(const Vec & p1, const Vec & m1, const Vec & p2, const Vec & m2)
 	: PolynomialEdge<3>(
-		   2*p1 +   m1 - 2*p2 + m2, // A
-		  -3*p1 - 2*m1 + 3*p2 - m2, // B
+		   p1,			    // D
 		   m1,			    // C
-		   p1			    // D
+		  -3*p1 - 2*m1 + 3*p2 - m2, // B
+		   2*p1 +   m1 - 2*p2 + m2  // A
 	  )
 { }
